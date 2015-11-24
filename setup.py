75be6e7d95ef6ee288d840d51bd108d643cce40d
Lukasz Mentel <lmmentel@gmail.com>
2015-11-24 23:34:04 +0100

    * restructured the code into a package   * created an espresso directory to hold the package   * renamed __init__.py to espresso.py   * moved all the python code into the espresso directory * modified the setup.py to use distutils instead of make to build the extensions   and install the package * added .gitignore * added MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,28 +1,64 @@
-#****************************************************************************
-# Copyright (C) 2013 SUNCAT
-# This file is distributed under the terms of the
-# GNU General Public License. See the file `COPYING'
-# in the root directory of the present distribution,
-# or http://www.gnu.org/copyleft/gpl.txt .
-#****************************************************************************
 
-from sys import argv,executable,exit,stderr
+# -*- coding: utf-8 -*-
 
-if len(argv)<2 or len(argv)>3 or argv[1]!='install' or len(argv)==3 and argv[2][:9]!='--prefix=':
-    print >>stderr, 'usage: '+executable+' '+argv[0]+' install [--prefix=installation-directory]'
-    exit(1)
+"""
+    Setup file for the ase-espresso package.
+"""
 
-from os import path
-from distutils.sysconfig import get_python_lib
-pylib = get_python_lib()
+from distutils.core import setup, Extension
+from distutils import ccompiler
+import shutil
+import os
 
-if len(argv)==3:
-    paths = [path.realpath(x) for x in [pylib,executable]]
-    prefix = path.commonprefix(paths)
-    instdir = argv[2][9:]
-    pylib = path.join(instdir,paths[0].replace(prefix,''))
-else:
-    instdir = path.normpath(path.join(pylib,'../../..'))
+MAIN_PACKAGE = "espresso"
+DESCRIPTION = "Python API for the Quantum Espresso software"
+LICENSE = "GPLv3"
+URL = "https://github.com/lmmentel/ase-espresso"
+AUTHOR = "Lukasz Mentel"
+EMAIL = "lmmentel@gmail.com"
+VERSION = '0.1.1'
 
-from os import system
-exit(system('make doinstall instdir='+instdir+' py='+executable+' pylib='+pylib))
+CLASSIFIERS = ['Development Status :: 3 - Alpha',
+               'Environment :: Console',
+               'Intended Audience :: Science/Research',
+               'License :: OSI Approved :: MIT License',
+               'Natural Language :: English',
+               'Programming Language :: Python',
+               'Programming Language :: Python :: 2',
+               'Topic :: Scientific/Engineering :: Chemistry',
+               'Topic :: Scientific/Engineering :: Physics']
+
+SCRIPTS = ['scripts/pwlog2trajectory']
+
+def readme():
+    '''Return the contents of the README.rst file.'''
+    with open('README.md') as freadme:
+        return freadme.read()
+
+cc = ccompiler.new_compiler()
+extensions = [Extension('espfilter', sources=['c-src/espfilter.c']),
+              Extension('cubecutperiodic', sources=['c-src/cubecutperiodic.c'])]
+
+for ext in extensions:
+    outfiles = cc.compile(ext.sources)
+    cc.link_executable(outfiles, ext.name)
+    shutil.move(ext.name, os.path.join('scripts', ext.name))
+    SCRIPTS.append(os.path.join('scripts', ext.name))
+
+def setup_package():
+
+    setup(name=MAIN_PACKAGE,
+          version=VERSION,
+          url=URL,
+          description=DESCRIPTION,
+          author=AUTHOR,
+          author_email=EMAIL,
+          license=LICENSE,
+          long_description=readme(),
+          classifiers=CLASSIFIERS,
+          packages=['espresso'],
+          scripts=SCRIPTS,
+    )
+
+if __name__ == "__main__":
+    setup_package()
