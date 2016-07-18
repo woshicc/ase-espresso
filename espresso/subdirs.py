9265012f0f6bebbd35f2d49fe2bfb7f1e5a3ac71
Lukasz Mentel <lmmentel@gmail.com>
2016-07-18 19:22:37 +0200

    added a string conversion to path and some small style corrections

--- espresso/subdirs.py
+++ espresso/subdirs.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
 
-#****************************************************************************
+# ****************************************************************************
 # Copyright (C) 2013 SUNCAT
 # This file is distributed under the terms of the
 # GNU General Public License. See the file `COPYING'
 # in the root directory of the present distribution,
 # or http://www.gnu.org/copyleft/gpl.txt .
-#****************************************************************************
+# ****************************************************************************
 
 'subroutines for creation of subdirectories & clean-up'
 
 from __future__ import print_function
-
-__version__ = '0.1.2'
 
 import os
 import tempfile
@@ -20,6 +18,9 @@ import string
 import random
 import subprocess
 from path import Path
+
+__version__ = '0.1.2'
+
 
 def mklocaltmp(workdir, site):
     '''
@@ -32,11 +33,12 @@ def mklocaltmp(workdir, site):
 
     if workdir is None or len(workdir) == 0:
         suffix = '_' + ''.join(random.choice(string.uppercase + string.digits) for _ in range(6))
-        tempdir = Path(tempfile.mkdtemp(prefix='qe' + site.jobid, suffix=suffix, dir=site.submitdir))
+        tempdir = Path(tempfile.mkdtemp(prefix='qe' + str(site.jobid), suffix=suffix, dir=site.submitdir))
     else:
         tempdir = Path(os.path.join(site.submitdir, workdir))
         tempdir.makedirs_p()
     return tempdir.abspath()
+
 
 def mkscratch(localtmp, site):
     '''
