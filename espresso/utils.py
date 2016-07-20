72939bcddffee96ef7e668ed6b4c577e157d2944
Lukasz Mentel <lmmentel@gmail.com>
2016-07-20 18:39:02 +0200

    replaced the specobj class wtith a namedtuple, added docstrings and updated the style

--- espresso/utils.py
+++ espresso/utils.py
@@ -1,62 +1,68 @@
 # -*- coding: utf-8 -*-
 
-#****************************************************************************
+# ****************************************************************************
 # Copyright (C) 2013 SUNCAT
 # This file is distributed under the terms of the
 # GNU General Public License. See the file `COPYING'
 # in the root directory of the present distribution,
 # or http://www.gnu.org/copyleft/gpl.txt .
-#****************************************************************************
+# ****************************************************************************
 
-__version__ = '0.1.2'
-
+from collections import namedtuple
 import numpy as np
 from ase import constraints
 
-class specobj:
-    #small species class containing the attributes of a species
-    def __init__(self, s='X', mass=0., magmom=0., U=0., J=0., U_alpha=0.):
-        self.s = s
-        self.mass = mass
-        self.magmom = magmom
-        self.U = U
-        self.J = J
-        self.U_alpha = U_alpha
+
+__version__ = '0.1.2'
 
 
-#add 'd0' to floating point number to avoid random trailing digits in Fortran input routines
+speciestuple = namedtuple('speciestuple',
+                          ['symbol', 'mass', 'magmom', 'U', 'J', 'U_alpha'])
+
+
 def num2str(x):
-    s = str(x)
-    if s.find('e')<0:
-        s += 'd0'
-    return s
+    '''
+    Add 'd00' to floating point number to avoid random trailing digits in
+    Fortran input routines
+    '''
 
-#convert python to fortran logical
+    if 'e' in str(x):
+        return str(x)
+    else:
+        return str(x) + 'd00'
+
+
 def bool2str(x):
+    'Convert python to fortran logical'
+
     if x:
         return '.true.'
     else:
         return '.false.'
 
-#convert some of ase's constraints to pw.x constraints for pw.x internal relaxation
-#returns constraints which are simply expressed as setting force components
-#as first list and other contraints that are implemented in espresso as
-#second list
+
 def convert_constraints(atoms):
+    '''
+    Convert some of ase's constraints to pw.x constraints for pw.x internal
+    relaxation returns constraints which are simply expressed as setting
+    force components as first list and other contraints that are
+    implemented in espresso as second list
+    '''
+
     if atoms.constraints:
         n = len(atoms)
-        if n==0:
-            return [],[]
+        if n == 0:
+            return [], []
         forcefilter = []
         otherconstr = []
         for c in atoms.constraints:
             if isinstance(c, constraints.FixAtoms):
-                if len(forcefilter)==0:
-                    forcefilter = np.ones((n,3), np.int)
-                forcefilter[c.index] = [0,0,0]
+                if len(forcefilter) == 0:
+                    forcefilter = np.ones((n, 3), np.int)
+                forcefilter[c.index] = [0, 0, 0]
             elif isinstance(c, constraints.FixCartesian):
-                if len(forcefilter)==0:
-                    forcefilter = np.ones((n,3), np.int)
+                if len(forcefilter) == 0:
+                    forcefilter = np.ones((n, 3), np.int)
                 forcefilter[c.a] = c.mask
             elif isinstance(c, constraints.FixBondLengths):
                 for d in c.constraints:
@@ -79,6 +85,6 @@ def convert_constraints(atoms):
             else:
                 raise NotImplementedError('constraint {} not implemented\n'
                     'consider ase-based relaxation with this constraint instead'.format(c.__name__))
-        return forcefilter,otherconstr
+        return forcefilter, otherconstr
     else:
-        return [],[]
+        return [], []
